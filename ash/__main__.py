de35015c1c6c00ff1f72db3f35d7a5f3472fe721
Martastain <martas@imm.cz>
2024-09-11 12:49:42 +0200

    chore: build script

--- ash/__main__.py
+++ ash/__main__.py
@@ -7,6 +7,7 @@ from .config import config
 from .health import get_health
 from .models import ServiceConfigModel, ServiceModel
 from .services import Services
+from .version import __version__
 
 
 def main():
@@ -51,6 +52,7 @@ def main():
 
 
 if __name__ == "__main__":
+    logging.info(f"Starting Ash v{__version__}")
     while "my guitar gently weeps":
         main()
         time.sleep(2)
